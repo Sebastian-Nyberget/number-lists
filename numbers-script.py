cdf5e82fdb44e5c70f0386c4cb70d4083a2139e7
Sebastian-Nyberget <nybesebe@gmail.com>
2025-03-21 11:14:23 +0100

    Python

---
+++ numbers-script.py
@@ -0,0 +1,7 @@
+with open('SmallTestList.txt', 'r') as file:
+    numbers = [int(line) for line in file]
+
+print(f"Sum: {sum(numbers)}")
+print(f"Gjennomsnitt: {sum(numbers) / len(numbers)}")
+print(f"Største verdi: {max(numbers)}")
+print(f"Minste verdi: {min(numbers)}")
